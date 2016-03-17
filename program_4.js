fafad1501cd9b35ee634e499b0b151c1b56b3b2c
Tim Meijome <reeebot@gmail.com>
2016-03-17 11:24:57 -0600

    Update program_4.js

--- program_4.js
+++ program_4.js
@@ -1,14 +1,9 @@
 var fs = require('fs')
 var path = require('path')
-var dir = process.argv[2]
-var ext = process.argv[3]
 
-
-fs.readdir(dir, function (err, files) {
+fs.readdir(process.argv[2], function (err, files) {
     if (err) {
         throw err; }
-    files.filter(function (file) {
-        return fs.statSync(file).isFile();
-    }).forEach(function (file) {
-        if ("." + ext == path.extname(file)) { console.log(file)}
+    files.forEach(function (file) {
+        if ("." + process.argv[3] === path.extname(file)) { console.log(file)}
 })});
