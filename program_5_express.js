85c76fc01aa61caeff9919e287c294f18ac01ea4
Tim Meijome <reeebot@gmail.com>
2016-04-07 09:36:43 -0600

    Update program_5_express.js

--- program_5_express.js
+++ program_5_express.js
@@ -1,15 +1,10 @@
 var express = require('express')
 var app = express()
+var bodyparser = require('body-parser')
+app.use(bodyparser.urlencoded({extended: false}))
 
-
-app.put('/message/:id', function(req, res){ 
-    var id = req.params.id
-    var str = require('crypto')
-        .createHash('sha1')
-        .update(new Date().toDateString() + id)
-        .digest('hex')
-    res.send(str)
-});
+app.use(express.static(process.argv[3]));
+app.use(require('stylus').middleware(__dirname + process.argv[3]))
 
 
 app.listen(process.argv[2])
